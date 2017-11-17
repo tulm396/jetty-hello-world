41ef4af215d06dd2d807016a173179b78fa0dfda
Le Minh Tu <tulm396@gmail.com>
2017-11-17 12:14:33 +0700

    Complete HelloWorld class

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,33 @@
+package helloworld;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class HelloWorld extends AbstractHandler {
+    @Override
+    public void handle( String target,
+                        Request baseRequest,
+                        HttpServletRequest request,
+                        HttpServletResponse response ) throws IOException,
+                                                      ServletException {
+        response.setContentType("text/html; charset=utf-8");
+
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        response.getWriter().println("<h1>Hello, world!</h1>");
+
+        baseRequest.setHandled(true);
+    }
+    public static void main(String a[]) throws Exception {
+        Server server = new Server(8080);
+        server.setHandler(new HelloWorld());
+
+        server.start();
+        server.join();
+    }
+}
